dfb8f85a9ee0dc47dae5d6a95dcf4bfa3e0d147b
Mariany Pinkston <marianypinkston@gmail.com>
2025-07-14 14:23:16 -0400

    Increase age locked notification time

--- src/main/java/dev/mariany/genesis/packet/clientbound/ClientboundPackets.java
+++ src/main/java/dev/mariany/genesis/packet/clientbound/ClientboundPackets.java
@@ -36,7 +36,7 @@ public class ClientboundPackets {
                         ),
                         null,
                         true,
-                        3000
+                        5000
                 ));
             }
         });
