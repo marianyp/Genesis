cfca5419021d412ac862217595262423aad6b44c
Mariany Pinkston <marianypinkston@gmail.com>
2025-08-07 19:17:57 -0400

    Remove redundant container click area and correct arguments for assembly click area

--- src/main/java/dev/mariany/genesis/compat/rei/client/GenesisClientREIPlugin.java
+++ src/main/java/dev/mariany/genesis/compat/rei/client/GenesisClientREIPlugin.java
@@ -1,6 +1,7 @@
 package dev.mariany.genesis.compat.rei.client;
 
 import dev.mariany.genesis.block.GenesisBlocks;
+import dev.mariany.genesis.client.gui.screen.ingame.AssemblyScreen;
 import dev.mariany.genesis.compat.rei.REICategoryIdentifiers;
 import dev.mariany.genesis.compat.rei.client.categories.crafting.AssemblyCategory;
 import me.shedaniel.math.Rectangle;
@@ -9,8 +10,6 @@ import me.shedaniel.rei.api.client.registry.category.CategoryRegistry;
 import me.shedaniel.rei.api.client.registry.screen.ScreenRegistry;
 import me.shedaniel.rei.api.common.util.EntryStacks;
 import me.shedaniel.rei.plugin.common.BuiltinPlugin;
-import net.minecraft.client.gui.screen.ingame.CraftingScreen;
-import net.minecraft.client.gui.screen.ingame.InventoryScreen;
 
 public class GenesisClientREIPlugin implements REIClientPlugin {
     @Override
@@ -24,14 +23,8 @@ public class GenesisClientREIPlugin implements REIClientPlugin {
     @Override
     public void registerScreens(ScreenRegistry registry) {
         registry.registerContainerClickArea(
-                new Rectangle(88, 32, 28, 23),
-                CraftingScreen.class,
-                REICategoryIdentifiers.ASSEMBLY
-        );
-
-        registry.registerContainerClickArea(
-                new Rectangle(137, 29, 10, 13),
-                InventoryScreen.class,
+                new Rectangle(111, 32, 28, 23),
+                AssemblyScreen.class,
                 REICategoryIdentifiers.ASSEMBLY
         );
     }
